9c9d74b04a2d89424c4b0585f394116468ecf423
P-Zone-GH <p.zone1019@gmail.com>
2022-10-06 15:19:26 +0800

    P

--- Python/File/test.py
+++ Python/File/test.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+from ast import For
+
+
 print("hello python")
 
 str='123456789'
@@ -13,8 +16,8 @@ print(str[2:])             # 输出从第三个开始后的所有字符
 print(str[1:5:2])          # 输出从第二个开始到第五个且每隔一个的字符（步长为2）
 print(str * 2)             # 输出字符串两次
 print(str + '你好')         # 连接字符串
- 
+
 print('------------------------------')
- 
-print('hello\nworld')      # 使用反斜杠(\)+n转义特殊字符
+
+print('hello\nworld')      #使用反斜杠(\)+n转义特殊字符
 print(r'hello \n world')     # 在字符串前面添加一个 r，表示原始字符串，不会发生转义
