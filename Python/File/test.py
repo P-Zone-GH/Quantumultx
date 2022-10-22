c1f75e05618fcbf854ba187b4560f0b387eb9c5d
P-Zone-GH <p.zone1019@gmail.com>
2022-10-22 13:25:54 +0800

    P

--- Python/File/test.py
+++ Python/File/test.py
@@ -8,8 +8,9 @@ print("hello python")
 str='123456789'
 print(str)                 # 输出字符串
 print(str[0:-1])           # 输出第一个到倒数第二个的所有字符
-
+print(str[0:-2])           # 输出第一个到倒数第三个的所有字符
 print(str[0])              # 输出字符串第一个字符
+
 print(str[2:5])            # 输出从第三个开始到第五个的字符
 
 print(str[2:])             # 输出从第三个开始后的所有字符
